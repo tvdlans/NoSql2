3c555eeeb6fd7c15f51d7927cfe5570c1cc6f49f
Steff Burgering <steffburgering@hotmail.com>
2020-02-28 10:57:21 +0100

    sdaeedc

--- Controller/View/Program.cs
+++ Controller/View/Program.cs
@@ -14,6 +14,7 @@ namespace View
         [STAThread]
         static void Main()
         {
+            asdckguawejcm,
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
